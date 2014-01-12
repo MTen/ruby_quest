4eacdda0ba8e382d833cdc49149d1eccf0639350
MTen <MichaelTener@gmail.com>
2014-01-12 15:05:04 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyQuest::Application.config.secret_token = 'bfcfb57f6f1e2ad1db26344b363151a02ac37b648427c563dd1d3e9413f31e8336013abacdc37b9c677c1d108e4c5eb39f40d1d35636f6e07e8f593ea5cb58f1'
